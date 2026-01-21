b8460256fb90f3d40d61c9feefd58ac6f124dbc7
ericriv1403 <edra2016omib@gmail.com>
2026-01-21 16:38:26 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Config global
-window.APP_CONFIG = {
-  backendUrl: "https://script.google.com/macros/s/AKfycbziorxxrEmLrHhCVQjE1zGDkfh7Nh_oZCtN_VYsd9AOjqvK0bzYvxfWDfpqBJKpBVnWlQ/exec",
-  requestTimeoutMs: 30000
-};
